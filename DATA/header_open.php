ec7f40db8ca9aef49c4dd453f021cfa9fe043825
jean1880 <jean18801880@gmail.com>
2013-12-03 22:31:00 -0500

    upload

---
+++ DATA/header_open.php
@@ -0,0 +1,10 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head profile="http://www.w3.org/2005/10/profile">
+    <link href="DATA/CSS/navStyle.css" rel="stylesheet" type="text/css"/>
+        <!-- This website was Designed, developed, and coded by Jean-Luc Desroches and Alex Barbosa -->
+        <?php 
+		include("DATA/GLOBALS.php");
+	?>
